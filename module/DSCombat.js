c027550d22dbd849d759de0e318050c93ab7c1bc
MalphasDev <85169766+MalphasDev@users.noreply.github.com>
2021-06-12 21:36:20 +0200

    Update DSCombat.js
    Funktionierender CombatTracker für DS

--- module/DSCombat.js
+++ module/DSCombat.js
@@ -7,73 +7,57 @@ export default class DSCombat extends Combat {
         console.log("Würfel: "+ a._actor.data.data.customroll.dice+"W10");
         console.log("Bonus: "+ a._actor.data.data.customroll.bonus);
         */
+
         const aeA = parseInt(a.initiative) || 0;
         const aeB = parseInt(b.initiative) || 0;
 
-        var sort = -1;
+        var sort = a.parent.getFlag('darkspace', 'combatStarted') ? 1 : -1; //holt sich vom parent (=combat) die info ob er begonnen hat, wird in startCombat gesetzt.
         console.log(sort);
         
         return ((aeA - aeB)*sort);
     }
 
     async startCombat() {
-        console.log("+++ startCombat +++")
         await this.setupTurns();
 
-        await this.rollAll();
-
+        //Muss unbeding aufgerufen werden bevor die ini gerollt wird
+        this.setFlag('darkspace', 'combatStarted', true);
         
-        
-        console.log("++++++++++");
-        var combatantlist_length = document.querySelectorAll(".combatant",".active").length;
-        var combatantlist = document.querySelectorAll(".combatant",".active")
-        var activeCombatant;
+        //console.log(this);
+        //console.log(this.combatants.values());
         /*
-        console.log(token);
-        debugger;
+        console.log(Array.from(this.combatants.values()));                        <--   Erstellt ein Array aus einer MAP des Combatant
+        console.log(Array.from(this.combatants.values()).map( (c) => {return {    <--   Holt die Einträge "combantantID" und "initiative" aus der Map und erstellt daraus eine Array
+            initiative: c.data.initiative,                                              https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Map
+            combatendId: c.data._id                                                     https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+        }} ));
 
-        
-        console.log("+++++ combatant +++++");
-        console.log(actor);
-        console.log("++++++++++");
         */
-        for(var i = 0; i < combatantlist_length; i++) {
-            //console.log(combatantlist[i].dataset.combatantId);
-            //console.log(combatantlist[i].className.includes("active"));
-            if(combatantlist[i].className.includes("active")) {
-                activeCombatant = combatantlist[i].dataset.combatantId;
-            }
-        }
-        
-        console.log(combatantlist);
-        console.log(activeCombatant);
-        var r = new Roll("2d10",{});
-        r.evaluate();
-        console.log(r);
-        console.log(r.terms);
-        console.log(r.result);
-        console.log(r.total);
-        
-        return this.updateCombatant ({
-            _id: activeCombatant,
-            initiative: r.total
-            
-        });
-        
-        
+        this.rollInitiative( Array.from(this.data.combatants.values()).filter( (c) => {
+            return c.data.initiative === undefined;
+        }).map( (c) => {  return c.data._id } ), {} )
 
+        let startAE = 1;
+        this.data.combatants.forEach( (c) => {
+            this.updateCombatant({
+                _id: c.data._id,
+                initiative: startAE++
+            });
+        })
 
-        
+        return this.update({round: 1, turn: 0});
     }
-    /*
+    
     rollInitiative(ids, options) {
-        console.log(ids);
-        console.log(options);
-
-        var ini = 1;
-        return Promise.Combat()
+        ids.forEach( (id) => {
+            this.updateCombatant({
+                _id: id,
+                initiative: new Roll("2d10",{}).evaluate().total
+            });
+        })
+        return this;
     }
-    */
+    
     async increaseAE(id, value) {
         
         var ae = this.combatant.initiative + value; // Aktuelle AE + AE-Kosten
